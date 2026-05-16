a6844e96139c07c2c29d95eff509a91488468e13
Neeraj <ettaneeraj2007@gmail.com>
2026-05-16 14:44:33 +0530

    Fixed deployment CORS

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express =
+  require("express");
 
-const mongoose = require("mongoose");
+const mongoose =
+  require("mongoose");
 
-const cors = require("cors");
+const cors =
+  require("cors");
 
-const dotenv = require("dotenv");
+require("dotenv").config();
 
 const authRoutes =
   require("./routes/authRoutes");
@@ -15,18 +18,29 @@ const taskRoutes =
 const uploadRoutes =
   require("./routes/uploadRoutes");
 
-dotenv.config();
-
 const app = express();
-
-app.use(cors());
 
 app.use(express.json());
 
-app.use(
-  "/uploads",
-  express.static("uploads")
-);
+app.use(cors());
+
+mongoose.connect(
+  process.env.MONGO_URI
+)
+
+.then(() => {
+
+  console.log(
+    "MongoDB Connected"
+  );
+
+})
+
+.catch((error) => {
+
+  console.log(error);
+
+});
 
 app.use(
   "/api/auth",
@@ -43,31 +57,21 @@ app.use(
   uploadRoutes
 );
 
+app.get("/", (req, res) => {
+
+  res.send(
+    "Backend Running"
+  );
+
+});
+
 const PORT =
   process.env.PORT || 5000;
 
-mongoose.connect(
-  process.env.MONGO_URI
-)
-
-.then(() => {
+app.listen(PORT, () => {
 
   console.log(
-    "MongoDB Connected"
+    `Server running on port ${PORT}`
   );
-
-  app.listen(PORT, () => {
-
-    console.log(
-      `Server running on port ${PORT}`
-    );
-
-  });
-
-})
-
-.catch((error) => {
-
-  console.log(error);
 
 });
