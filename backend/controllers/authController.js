38d18467ad6b41b81d97d987b08a59f636d65df2
Neeraj <ettaneeraj2007@gmail.com>
2026-05-16 15:37:29 +0530

    Fixed JWT token user id

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,37 +7,47 @@ const bcrypt =
 const jwt =
   require("jsonwebtoken");
 
-exports.registerUser =
+const register =
   async (req, res) => {
 
     try {
 
       const {
+
         email,
+
         password,
+
       } = req.body;
 
       const existingUser =
         await User.findOne({
+
           email,
+
         });
 
       if (existingUser) {
 
         return res.status(400).json({
+
           message:
             "User already exists",
+
         });
 
       }
 
       const hashedPassword =
         await bcrypt.hash(
+
           password,
+
           10
+
         );
 
-      const user =
+      const newUser =
         await User.create({
 
           email,
@@ -48,60 +58,72 @@ exports.registerUser =
         });
 
       res.status(201).json({
+
         message:
           "User registered successfully",
 
-        user,
       });
 
     } catch (error) {
 
-      console.log(error);
-
       res.status(500).json({
+
         message:
           "Registration failed",
+
       });
 
     }
 
   };
 
-exports.loginUser =
+const login =
   async (req, res) => {
 
     try {
 
       const {
+
         email,
+
         password,
+
       } = req.body;
 
       const user =
         await User.findOne({
+
           email,
+
         });
 
       if (!user) {
 
         return res.status(400).json({
+
           message:
             "Invalid credentials",
+
         });
 
       }
 
-      const validPassword =
+      const isMatch =
         await bcrypt.compare(
+
           password,
+
           user.password
+
         );
 
-      if (!validPassword) {
+      if (!isMatch) {
 
         return res.status(400).json({
+
           message:
             "Invalid credentials",
+
         });
 
       }
@@ -110,37 +132,57 @@ exports.loginUser =
         jwt.sign(
 
           {
+
             id: user._id,
+
+            email:
+              user.email,
+
           },
 
-          "secretkey",
+          process.env.JWT_SECRET,
 
           {
-            expiresIn: "1d",
+
+            expiresIn:
+              "7d",
+
           }
 
         );
 
-      res.status(200).json({
+      res.json({
 
         token,
 
         user: {
+
           id: user._id,
-          email: user.email,
+
+          email:
+            user.email,
+
         },
 
       });
 
     } catch (error) {
 
-      console.log(error);
-
       res.status(500).json({
+
         message:
           "Login failed",
+
       });
 
     }
 
   };
+
+module.exports = {
+
+  register,
+
+  login,
+
+};
